f1c3757a1c0081913373c43068a05f98536b8cfa
vovanduc <vovanduc1989@gmail.com>
2016-03-25 16:42:10 +0700

    Update

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -25,12 +25,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $temp = ['fe35b4e3-c8ca-49f1-a3ee-e396584c1461','a4aa0059-1253-48e6-b42f-fdcfa926a688','1da4ab16-6dc4-4cb9-b509-38b0c8e1685d'];
-
-        $result = \Package::where('deleted', 0)->get();
-        foreach($result as $item) {
-            \Package::where('uuid', $item->uuid)->update(['status' => rand(1,6), 'location_id' => $temp[rand(0,2)]]);
-        }
+        // $temp = ['fe35b4e3-c8ca-49f1-a3ee-e396584c1461','a4aa0059-1253-48e6-b42f-fdcfa926a688','1da4ab16-6dc4-4cb9-b509-38b0c8e1685d'];
+        //
+        // $result = \Package::where('deleted', 0)->get();
+        // foreach($result as $item) {
+        //     \Package::where('uuid', $item->uuid)->update(['status' => rand(1,6), 'location_id' => $temp[rand(0,2)]]);
+        // }
 
         $sum = array();
         for ($i=1; $i<=6 ; $i++) {
