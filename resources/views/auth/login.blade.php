aa770d440e8eadb2f2bfb32cf6e3fc6f04805360
Lee Bin <vovanduc1989@gmail.com>
2016-04-03 21:37:52 +0700

    Update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">Khu vực đăng nhập dành cho người quản lý hệ thống</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {!! csrf_field() !!}
