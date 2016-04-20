df581db9521c48367031040152afca450dff45a7
vovanduc <vovanduc1989@gmail.com>
2016-04-20 09:33:42 +0700

    Update

--- app/Http/Controllers/Admin/ImportsController.php
+++ app/Http/Controllers/Admin/ImportsController.php
@@ -19,7 +19,7 @@ class ImportsController extends Controller
 
     public function excel()
     {
-        //print 'Hello';exit;
+        print 'Hello';exit;
 
         require_once base_path('vendor/faisalman/simple-excel-php/src/SimpleExcel/SimpleExcel.php');
         $excel = new \SimpleExcel\SimpleExcel('CSV');
