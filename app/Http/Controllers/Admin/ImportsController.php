8be7b36d5ff76cbba1d0f93991ec791677fc903a
vovanduc <vovanduc1989@gmail.com>
2016-05-19 15:47:45 +0700

    Update

--- app/Http/Controllers/Admin/ImportsController.php
+++ app/Http/Controllers/Admin/ImportsController.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\Controller;
 class ImportsController extends Controller
 {
     protected $status = 2;
-    protected $shipment_id = '180-95172136';
-    protected $date = '2016-05-06';
+    protected $shipment_id = '180-95204384';
+    protected $date = '2016-05-10';
 
     public function __construct(Request $request)
     {
@@ -20,15 +20,15 @@ class ImportsController extends Controller
 
     public function excel()
     {
-        print 'Hello';exit;
+        //print 'Hello';exit;
 
         require_once base_path('vendor/faisalman/simple-excel-php/src/SimpleExcel/SimpleExcel.php');
         $excel = new \SimpleExcel\SimpleExcel('CSV');
 
         ############################################################################
-        $excel->parser->loadFile(base_path('public/assets/admin/excel/06.05.20161.csv'));
+        $excel->parser->loadFile(base_path('public/assets/admin/excel/10.05.20161.csv'));
         $count = 1;
-        for ($i=9; $i <= 217 ; $i++) {
+        for ($i=9; $i <= 219 ; $i++) {
             for ($k=1; $k <= 15 ; $k++) {
                 $string = $excel->parser->getCell($i,$k);
                 if ($k==2) $invoice = $excel->parser->getCell($i,$k);
